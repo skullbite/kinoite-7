6678f2841b79cf1a4464983aa892957549d352b8
skellypupper <git@bork.quest>
2026-02-07 13:24:44 -0500

    No more plymouthvista, it workn't

--- build_files/build.sh
+++ build_files/build.sh
@@ -251,39 +251,40 @@ cp $CUR/misc/branding/kcminfo.png /usr/share/fed7/logo.png
 sed -i "s/Theme=bgrt/Theme=PlymouthVista/g" /usr/share/plymouth/plymouthd.defaults
 sed -i "s/#Current=01-breeze-fedora/Current=sddm-theme-mod/g" /etc/sddm.conf
 # TODO: Install script is very much user-level.
-git clone https://github.com/furkrn/PlymouthVista /tmp/PlymouthVista
-CUR=/tmp/PlymouthVista
-cd $CUR
-sh compile.sh
- dracut --force --omit plymouth --regenerate-all --verbose
-chmod +x PlymouthVista.script
+# git clone https://github.com/furkrn/PlymouthVista /tmp/PlymouthVista
+# CUR=/tmp/PlymouthVista
+# cd $CUR
+# sh compile.sh
+# dracut --force --omit plymouth --regenerate-all --verbose
+# chmod +x PlymouthVista.script
 
-cp $CUR/lucon_disable_anti_aliasing.conf /etc/fonts/conf.d/10-lucon_disable_anti_aliasing.conf
-sh gen_blur.sh
+# cp $CUR/lucon_disable_anti_aliasing.conf /etc/fonts/conf.d/10-lucon_disable_anti_aliasing.conf
+# sh gen_blur.sh
 
-cp $CUR/systemd/system/update-plymouth-vista-state-boot.service /etc/systemd/system
-cp $CUR/systemd/system/update-plymouth-vista-state-quit.service /etc/systemd/system
-cp $CUR/systemd/hibernation/plymouth-vista-hibernate.service /etc/systemd/system
-cp $CUR/systemd/hibernation/plymouth-vista-resume-from-hibernation.service /etc/systemd/system
+# cp $CUR/systemd/system/update-plymouth-vista-state-boot.service /etc/systemd/system
+# cp $CUR/systemd/system/update-plymouth-vista-state-quit.service /etc/systemd/system
+# cp $CUR/systemd/hibernation/plymouth-vista-hibernate.service /etc/systemd/system
+# cp $CUR/systemd/hibernation/plymouth-vista-resume-from-hibernation.service /etc/systemd/system
 
-cp $CUR/systemd/user/update-plymouth-vista-state-logon.service /etc/systemd/user
+# cp $CUR/systemd/user/update-plymouth-vista-state-logon.service /etc/systemd/user
 
 
 # TODO: Bound this variable to a config value, either thru 7just or system settings
-sed "s/\$\(.*\)\"/10\"/g" -i systemd/slowdown/plymouth-vista-slow-boot-animation.service
-cat systemd/slowdown/plymouth-vista-slow-boot-animation.service
-cp systemd/slowdown/plymouth-vista-slow-boot-animation.service /etc/systemd/system
+# sed "s/\$\(.*\)\"/10\"/g" -i systemd/slowdown/plymouth-vista-slow-boot-animation.service
+# cat systemd/slowdown/plymouth-vista-slow-boot-animation.service
+# cp systemd/slowdown/plymouth-vista-slow-boot-animation.service /etc/systemd/system
 
-systemctl enable update-plymouth-vista-state-{boot,quit}.service
-systemctl enable plymouth-vista-{hibernate,resume-from-hibernation}.service
-systemctl enable plymouth-vista-slow-boot-animation.service
+# systemctl enable update-plymouth-vista-state-{boot,quit}.service
+# systemctl enable plymouth-vista-{hibernate,resume-from-hibernation}.service
+# systemctl enable plymouth-vista-slow-boot-animation.service
  
 cp -r $(pwd) /usr/share/plymouth/themes/PlymouthVista
 # chmod +x ./compile.sh
 # chmod +x ./install.sh
 # ./compile.sh
 # ./install.sh -s -q
-plymouth-set-default-theme -R PlymouthVista
+# TODO: d
+# plymouth-set-default-theme -R PlymouthVista
 rm /usr/share/wayland-sessions/plasma.desktop
 
 systemctl enable podman.socket
