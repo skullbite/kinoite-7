567232090d4be1d444f420aed7eb9c7301c44f4a
skullbite <git@bork.quest>
2026-02-12 14:22:49 -0500

    attempt to fix motd + more config in 7just

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ rsync -rvK /ctx/sys/ /
 python /ctx/update_os_release.py
 
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-dnf install -y fastfetch steam tailscale gum
+dnf install -y fastfetch steam tailscale gum glow
 
 cat > /usr/share/ublue-os/image-info.json <<EOF
 {
--- system_files/etc/profile.d/kinoite7.sh
+++ system_files/etc/profile.d/kinoite7.sh
@@ -2,7 +2,6 @@
 case $(7just _get-config bash-profile cmdprompt) in
     cmdprompt)
         echo -e "Fedora Kinoite [Version $(uname -r)]\nCopyright (c) $(date +%Y) Fedora Community. All rights reserved.\n"
-        PS1='C:${PWD//\//\\\\}> '
         ;;
     ublue)
         /usr/libexec/ublue-motd
@@ -13,3 +12,6 @@ case $(7just _get-config bash-profile cmdprompt) in
 esac
 
 
+if [[ $(7just _get-config win-7-prompt) == "1" ]]; then
+    PS1='C:${PWD//\//\\\\}> '
+fi
