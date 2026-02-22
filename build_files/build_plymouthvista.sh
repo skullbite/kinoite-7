38773ef24ac1a76c6dbb19cedabb855fb8e24d6d
skullbite <git@bork.quest>
2026-02-22 15:07:50 -0500

    uncat

--- build_files/build_plymouthvista.sh
+++ build_files/build_plymouthvista.sh
@@ -15,8 +15,7 @@ git clone https://github.com/furkrn/PlymouthVista
 cd PlymouthVista
 
 sed -i "s/vista\";/7\";/g" src/plymouth_config.sp
-cat src/plymouth_config.sp
 sh compile.sh 
 sh install.sh -s
 
-dracut --force --verbose
+# dracut --force --verbose
