b87c5d19e6f3a55ac593567d9f3d656eb6583ca9
skullbite <git@bork.quest>
2026-02-23 09:14:31 -0500

    take 78

--- build_files/build_plymouthvista.sh
+++ build_files/build_plymouthvista.sh
@@ -15,5 +15,5 @@ sh compile.sh
 sh install.sh -s
 
 export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${FEDORA_KERNEL}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
