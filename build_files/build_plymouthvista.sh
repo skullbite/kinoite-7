feb344bef5ecce7de99e545b3a27aabd3edba684
skullbite <git@bork.quest>
2026-02-23 10:24:15 -0500

    configure plymouthvista

--- build_files/build_plymouthvista.sh
+++ build_files/build_plymouthvista.sh
@@ -11,6 +11,11 @@ git clone https://github.com/furkrn/PlymouthVista
 cd PlymouthVista
 
 sed -i "s/vista\";/7\";/g" src/plymouth_config.sp
+sed -i "s/UseLegacyBootScreen = 1/UseLegacyBootScreen = 0/g" src/plymouth_config.sp
+sed -i "s/UseShadow = 0/UseShadow = 1/g" src/plymouth_config.sp
+sed -i "s/BootSlowdown = 0/BootSlowdown = 10/g" src/plymouth_config.sp
+sed -i "s/Starting Windows/Starting Kinoite/g" src/plymouth_config.sp
+sed -i "s/Microsoft Corporation/Fedora Community/g" src/plymouth_config.sp
 sh compile.sh 
 sh install.sh -s
 
