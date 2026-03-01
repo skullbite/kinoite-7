f64480879215fd4b478c6324fca4215d3ccaa5a8
skullbite <git@bork.quest>
2026-03-01 13:21:43 -0500

    do second kernel compile to fix copyright symbol

--- build_files/build_plymouthvista.sh
+++ build_files/build_plymouthvista.sh
@@ -22,3 +22,7 @@ sh install.sh -s
 export DRACUT_NO_XATTR=1
 /usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+sh omitPlymouth.sh
+
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
