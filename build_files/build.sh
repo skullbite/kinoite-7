b37a21784905249f6982c6d6e6d2c9e5741ff2c6
skullbite <git@bork.quest>
2026-03-01 13:23:36 -0500

    add 7just suggestions (ty zirconium)

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,8 @@ set -ouex pipefail
 rsync -rvK /ctx/sys/ /
 python /ctx/update_os_release.py
 
+# sed -i "s/#Session=/Session=AeroThemePlasma.desktop/g" /etc/sddm.conf
+
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 dnf install -y fastfetch steam tailscale gum glow
 
@@ -22,8 +24,10 @@ dnf install -y fastfetch steam tailscale gum glow
 # }
 # EOF
 
-7just --completions bash > /etc/bash_completion.d/7just
-chmod +x /etc/bash_completion.d/7just
+install -d /usr/share/bash-completion/completions /usr/share/zsh/site-functions /usr/share/fish/vendor_completions.d/
+7just --completions bash | sed -E 's/([\(_" ])just/\17just/g' > /usr/share/bash-completion/completions/7just
+7just --completions zsh | sed -E 's/([\(_" ])just/\17just/g' > /usr/share/zsh/site-functions/_7just
+7just --completions fish | sed -E 's/([\(_" ])just/\17just/g' > /usr/share/fish/vendor_completions.d/7just.fish
 
 systemctl enable podman.socket
 systemctl enable kin7-init.service
